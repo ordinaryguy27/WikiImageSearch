50c11dd7270cb89b4f3e6bbfa26ac71442286fa7
darshan <darshanpatil27@gmail.com>
2016-06-12 21:47:37 +0530

    Added condition in EditorActionListener
    Signed-off-by: darshan <darshanpatil27@gmail.com>

--- app/src/main/java/com/darshan/wikiimagesearch/MainActivity.java
+++ app/src/main/java/com/darshan/wikiimagesearch/MainActivity.java
@@ -14,6 +14,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -52,8 +53,11 @@ public class MainActivity extends Activity {
         mSearchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                buttonpressed(mSearchBox);
-                return true;
+                if ((event != null && (event.getKeyCode() == KeyEvent.KEYCODE_ENTER)) ||
+                        (actionId == EditorInfo.IME_ACTION_DONE)) {
+                    buttonpressed(mSearchBox);
+                }
+                return false;
             }
         });
         RecyclerView recycleView = (RecyclerView) findViewById(R.id.recycleview);
